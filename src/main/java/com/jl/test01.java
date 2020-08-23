3b9e30fd1a219ad0f6f0071c69c59865a0468fcb
Jeremy-Lin-Dev <jeremyshantouit@163.com>
2020-08-23 15:08:30 +0800

    TestB

--- src/main/java/com/jl/test01.java
+++ src/main/java/com/jl/test01.java
@@ -3,6 +3,6 @@ package com.jl;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("打包成功");
-        System.out.println("TestA");
+        System.out.println("TestB");
     }
 }
